911bbfda307e5dcf7b366b928e5b3c380ebfc989
Alfarizy <rizy.izy15@gmail.com>
2024-10-12 18:06:23 +0200

    Initial Commit

---
+++ ThreadsAppClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsAppCloneTests/ThreadsAppCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsAppCloneTests.swift
+//  ThreadsAppCloneTests
+//
+//  Created by Alfarizy on 10/12/24.
+//
+
+import Testing
+@testable import ThreadsAppClone
+
+struct ThreadsAppCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
