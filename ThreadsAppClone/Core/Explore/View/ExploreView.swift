9036b301acb2c88d69ffcb5b4f5f05ce38ae6fb3
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:15:43 +0200

    Add view for the explore page

---
+++ ThreadsAppClone/Core/Explore/View/ExploreView.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/16/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { user in
+                        VStack {
+                            UserCell()
+                            
+                            Divider()
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Search")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
