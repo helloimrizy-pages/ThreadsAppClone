035d4dfcdec3c7fd668d548eb439bb8f28e40e0a
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:14:04 +0200

    Add view for the feed/home page

---
+++ ThreadsAppClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,43 @@
+//
+//  FeedView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/15/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: Refresh threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        FeedView()
+    }
+}
---
+++ ThreadsAppClone/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,75 @@
+//
+//  ThreadCell.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/16/24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                CircularProfileImageView()
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("helloimrizy")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.footnote)
+                            .foregroundColor(Color(.systemGray3))
+                        
+                        Button {
+    
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("Hi, I am Rizy!")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
