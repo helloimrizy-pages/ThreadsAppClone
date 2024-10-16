3d36e301bb08343c45dcd19e0e7396208f38e315
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:14:23 +0200

    Add profile view

---
+++ ThreadsAppClone/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/16/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile view")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
