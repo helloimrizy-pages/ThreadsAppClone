7e66cdd1ed668a0430ea658f15dcc55f8ab33700
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:11:48 +0200

    Move the file into App folder

---
+++ ThreadsAppClone/App/ThreadsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsAppCloneApp.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
