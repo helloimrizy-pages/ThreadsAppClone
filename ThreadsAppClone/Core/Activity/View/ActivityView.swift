4ef0b3942f1dbe96f4f1d72e111e6884936b8845
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:16:44 +0200

    Add a view for the activity page

---
+++ ThreadsAppClone/Core/Activity/View/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/16/24.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text("Activity view")
+    }
+}
+
+#Preview {
+    ActivityView()
+}
