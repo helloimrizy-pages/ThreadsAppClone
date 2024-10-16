917d6de9dd4790590dbe790e2c62f875539785dd
Alfarizy <rizy.izy15@gmail.com>
2024-10-16 02:15:04 +0200

    Add a view for creating thread/post

---
+++ ThreadsAppClone/Core/ThreadCreation/View/CreateThreadView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateThreadView.swift
+//  ThreadsAppClone
+//
+//  Created by Alfarizy on 10/16/24.
+//
+
+import SwiftUI
+
+struct CreateThreadView: View {
+    var body: some View {
+        Text("Create thread view")
+    }
+}
+
+#Preview {
+    CreateThreadView()
+}
